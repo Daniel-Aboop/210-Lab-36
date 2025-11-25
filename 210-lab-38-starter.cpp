b2d8623f4db52848887a8399dc8d5a924f722469
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-11-24 16:04:45 -0800

    Milestone 4:04 PM

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -24,18 +24,44 @@ int main() {
         tree.insertNode(line);
     }
     int choice;
+    string input;
     do{
         cout<<"Record functions:"<<endl;
         cout<<"[1]Insert Code"<<endl;
         cout<<"[2]Is code in records"<<endl;
         cout<<"[3]Remove code from record"<<endl;
-        cout<<"[4]Print out BST Preorder "<<endl;
+        cout<<"[4]Print out BST Preorder <-this one is very long "<<endl;
         cout<<"[5]Exit Record functions"<<endl;
         cin>>choice;
         switch(choice){
             case 1:
+                cout<<"Enter code to add to the records"<<endl;
+                cin>>input;
+                tree.insertNode(input);
+                cout<<"Added to the records!"<<endl;
                 break;
             case 2:
+                cout<<"Enter code to search for:"<<endl; 
+                cin>>input;
+                if(tree.searchNode(input)){
+                    cout<<"Code is inside of the records!"<<endl;
+                }
+                else{
+                    cout<<"Code is not inside of the records!"<<endl;
+                }
+                break;
+            case 3:
+                cout<<"Enter code to remove:"<<endl;
+                cin>>input;
+                tree.remove(input);
+                cout<<"Code removed"<<endl;
+                break;
+            case 4:
+                tree.displayPreOrder();
+                break;
+            case 5:
+                cout<<"Exiting...";
+                break;
 
         }
     }while(choice!=5);
